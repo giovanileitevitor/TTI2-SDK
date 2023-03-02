da435ab3396e19a93bb246512997ff202485f367
Felippe Ferreira <felippe.ferreira@bbnk.com.br>
2023-03-02 18:22:17 +0000

    criacao de home fragment

--- tti2sdk/src/main/java/com/timwe/tti2sdk/ui/fragments/HomeFragment.kt
+++ tti2sdk/src/main/java/com/timwe/tti2sdk/ui/fragments/HomeFragment.kt
@@ -1,14 +1,18 @@
 package com.timwe.tti2sdk.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.timwe.tti2sdk.databinding.FragmentHomeBinding
+import com.timwe.tti2sdk.ui.Navigation
+import com.timwe.tti2sdk.ui.home.HomeActivity
 
 class HomeFragment: BaseFragment() {
 
-    private var binding : FragmentHomeBinding ? = null
+    private lateinit var binding : FragmentHomeBinding
 
     companion object HomeStats {
         fun newInstance(): HomeFragment {
@@ -22,13 +26,33 @@ class HomeFragment: BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container,false);
-        val view = binding?.root;
+        val view = binding.root;
         return view;
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.iconMissions.setOnClickListener {
+            Log.i("MISSIONS", "click")
+            val fragment: BaseFragment = Navigation.getFragmentFromFragmentId(Navigation.FragmentId.MISSIONS)
+            ((activity) as HomeActivity).showNewFragmentAndAddToSystemBackStack(fragment)
+        }
+
+        binding.iconPrizes.setOnClickListener {
+            Log.i("PRIZES", "click")
+        }
+
+        binding.iconTeam.setOnClickListener{
+            Log.i("TEAM", "click")
+        }
+
+        binding.topHome.nameAvatar.text = "Felippe dos Santos Ferreira"
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        binding = null
     }
 
 }
