5075fdbc059c6770d1670f88c40cbb07f6a7b017
Giovani Vitor <giovani.vitor@timwetech.com>
2023-03-02 16:59:11 -0300

    added dependency injection files and structure

---
+++ tti2sdk/src/main/java/com/timwe/tti2sdk/di/AppComponent.kt
@@ -0,0 +1,21 @@
+package com.timwe.tti2sdk.di
+
+import com.timwe.tti2sdk.di.AppModules.dataModules
+import com.timwe.tti2sdk.di.AppModules.domainModules
+import com.timwe.tti2sdk.di.AppModules.otherModules
+import com.timwe.tti2sdk.di.AppModules.presentationModules
+import org.koin.core.module.Module
+
+object AppComponent {
+
+    fun getAllModules(): List<Module> = listOf(*getAppModules())
+
+    private fun getAppModules(): Array<Module>{
+        return arrayOf(
+            presentationModules,
+            domainModules,
+            dataModules,
+            otherModules
+        )
+    }
+}
