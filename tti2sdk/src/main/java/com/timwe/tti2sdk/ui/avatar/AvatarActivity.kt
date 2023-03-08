08c10302b3a711f84577d93d97c0b202fab2203a
Giovani Vitor <giovani.vitor@timwetech.com>
2023-03-07 23:03:09 -0300

    Added topbar icons

--- tti2sdk/src/main/java/com/timwe/tti2sdk/ui/avatar/AvatarActivity.kt
+++ tti2sdk/src/main/java/com/timwe/tti2sdk/ui/avatar/AvatarActivity.kt
@@ -1,19 +1,24 @@
 package com.timwe.tti2sdk.ui.avatar
 
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import com.airbnb.lottie.LottieDrawable
+import com.timwe.tti2sdk.R
 import com.timwe.tti2sdk.databinding.ActivityAvatarBinding
 
 class AvatarActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityAvatarBinding
+    //private val viewModel: AvatarViewModel by viewModel()
+    private  val rotateElement : Animation by lazy { AnimationUtils.loadAnimation(this, R.anim.rotate_anim) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAvatarBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
-
     }
 
     override fun onResume() {
@@ -22,15 +27,35 @@ class AvatarActivity: AppCompatActivity() {
         setupListeners()
     }
 
-    private fun setupView(){
+    private fun setupView() = with(binding){
 
     }
 
     private fun setupListeners(){
+        binding.btnBackAvatar.setOnClickListener {
+            super.onBackPressed()
+
+        }
+
+        binding.btnShareAvatar.setOnClickListener {
+
+        }
+
+        binding.btnSaveAvatar.setOnClickListener {
+
+        }
+        binding.btnRandomize.setOnClickListener{
+            startingIconAnimation()
+            //Do the action to randomize avatar
+        }
 
     }
 
     private fun setupObservers(){
 
+    }
+
+    private fun startingIconAnimation() = with(binding){
+        imgRandomize.startAnimation(rotateElement)
     }
 }
