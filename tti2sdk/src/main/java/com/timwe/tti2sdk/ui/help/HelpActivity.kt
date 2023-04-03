ec9ea32a4ed9012d1bb85392e983a551c0d78bec
Bruno Felix <57107515+BrunoFelix98@users.noreply.github.com>
2023-04-03 16:34:43 +0100

    Todos os fragmentos feitos

--- tti2sdk/src/main/java/com/timwe/tti2sdk/ui/help/HelpActivity.kt
+++ tti2sdk/src/main/java/com/timwe/tti2sdk/ui/help/HelpActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.timwe.tti2sdk.databinding.ActivityHelpBinding
 import com.timwe.utils.onDebouncedListener
+import org.koin.android.ext.android.bind
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HelpActivity: AppCompatActivity() {
@@ -16,6 +17,17 @@ class HelpActivity: AppCompatActivity() {
         binding = ActivityHelpBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setupView()
+
+        val adapter = HelpViewPagerAdapter(supportFragmentManager)
+
+        adapter.addFragment(HelpMissionsFragment())
+        adapter.addFragment(HelpPrizesFragment())
+        adapter.addFragment(HelpRupiahFragment())
+        adapter.addFragment(HelpEventsFragment())
+        adapter.addFragment(HelpTierRewardsFragment())
+
+        //binding.helpViewPager.adapter = adapter
+        //binding.helpTabLayout.setupWithViewPager(binding.helpViewPager)
     }
 
     override fun onResume() {
