383d60927eff6591d440aaeb0bf237de42fb1c20
Giovani Vitor <giovani.vitor@timwetech.com>
2023-04-12 10:28:21 -0300

    fix map home

--- tti2sdk/src/main/java/com/timwe/tti2sdk/data/net/mapper/CityInfoResponseToCityInfo.kt
+++ tti2sdk/src/main/java/com/timwe/tti2sdk/data/net/mapper/CityInfoResponseToCityInfo.kt
@@ -7,6 +7,7 @@ import com.timwe.tti2sdk.data.net.data.Mapper
 class CityInfoResponseToCityInfo : Mapper<CityInfoResponse, CityInfo>(){
 
     override fun transform(item: CityInfoResponse): CityInfo {
+        //TODO - terminar de modelar o mapeamento das informaçoes usadas na tela DestinationActivity
 
         return CityInfo(
             id = item.id
