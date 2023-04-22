87e18bc6a6e0562521b71695311e93987286e3a7
Giovani Leite Vitor <giovani.vitor@timwe.com>
2023-04-22 13:35:26 -0300

    fix destination flow

--- tti2sdk/src/main/java/com/timwe/tti2sdk/ui/destinations/adapters/PlaceAdapter.kt
+++ tti2sdk/src/main/java/com/timwe/tti2sdk/ui/destinations/adapters/PlaceAdapter.kt
@@ -1,24 +1,26 @@
 package com.timwe.tti2sdk.ui.destinations.adapters
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.RequestManager
 import com.timwe.tti2sdk.R
 import com.timwe.tti2sdk.data.model.response.Wikipedia
 
 class PlaceAdapter(
-    private var data: List<Wikipedia> = emptyList(),
+    private val context: Context,
+    private var data: List<Wikipedia>,
     private val mGlide: RequestManager,
     private val itemListener: (Wikipedia) -> Unit
 ): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-
         val view: View = inflater.inflate(R.layout.item_around_here, parent, false)
         return DefaultVH(view)
     }
@@ -32,12 +34,14 @@ class PlaceAdapter(
     }
 
     private inner class DefaultVH(itemView: View): RecyclerView.ViewHolder(itemView){
+        var containerPlace: CardView = itemView.findViewById(R.id.containerPlace)
         var imgAroundHere: ImageView = itemView.findViewById(R.id.imgAroundHere)
         val type: TextView = itemView.findViewById(R.id.txtType)
         val placeName: TextView = itemView.findViewById(R.id.txtNamePlace)
+        val typeIcon: ImageView = itemView.findViewById(R.id.imgTypeIcon)
 
         init{
-            itemView.setOnClickListener {
+            containerPlace.setOnClickListener {
                 val position = bindingAdapterPosition
                 val item = data[position]
                 itemListener.invoke(item)
@@ -49,11 +53,21 @@ class PlaceAdapter(
         val item = data[position]
         val defaultVH = holder as DefaultVH
 
-        mGlide.load(item.imageUrl)
+        mGlide
+            .load(item.imageUrl)
             .into(defaultVH.imgAroundHere)
 
+        defaultVH.imgAroundHere.scaleType = ImageView.ScaleType.CENTER_CROP
         defaultVH.type.text = item.category
         defaultVH.placeName.text = item.name
+
+        when(item.category){
+            "All"-> { defaultVH.typeIcon.setImageResource(R.drawable.ic_all) }
+            "FOOD" -> { defaultVH.typeIcon.setImageResource(R.drawable.ic_foods)}
+            "SIGHTS" -> { defaultVH.typeIcon.setImageResource(R.drawable.ic_sights)}
+            "STAYS" -> { defaultVH.typeIcon.setImageResource(R.drawable.ic_stays)}
+            else -> { defaultVH.typeIcon.setImageResource(R.drawable.ic_all)}
+        }
     }
 
     fun replaceItens(newData: List<Wikipedia>){
