cab9160aa123f03721b01c0c737e1a3d469c33ed
Giovani Leite Vitor <giovani.vitor@timwe.com>
2023-06-23 13:15:16 -0500

    ajuste da baseActivity

--- app/src/main/java/com/timwe/demo/MainActivityCallSdk.kt
+++ app/src/main/java/com/timwe/demo/MainActivityCallSdk.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.timwe.demo.databinding.ActivityMainBinding
 import com.timwe.init.*
+
 class MainActivityCallSdk : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
